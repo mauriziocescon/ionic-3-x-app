63f0d4e353500515a37d980c4a79b3d77df6a9d6
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-05-25 12:53:19 +0200

    Bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 
 import { Platform } from "ionic-angular";
-import { StatusBar, Splashscreen } from "ionic-native";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 import { TranslateService } from "ng2-translate";
 
 import { TabsComponent } from "./tabs/tabs.module";
@@ -12,14 +13,14 @@ import { TabsComponent } from "./tabs/tabs.module";
 export class AppComponent {
   public rootPage: any;
 
-  constructor(platform: Platform, translate: TranslateService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
     this.rootPage = TabsComponent;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
 
       translate.setDefaultLang("en");
       translate.use("en");
