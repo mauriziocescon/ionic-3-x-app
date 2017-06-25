4868a97511e5f19203ff377a59a1f533e0fd9f3e
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-06-25 23:58:44 +0200

    bug fixes

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-Todo-list built on ionic 3.x, angular, ES2017
+Todo-list built on ionic 3.x, angular, ES2017 (Beta)
 =========
 
-A simple todo-list application
+## How to build the app
+
+1. Download and install [NodeJS](https://nodejs.org/en/)
+
+2. From the console, run ``npm install``
+
+3. On the console, run ``npm run build`` in order to build the code inside *dist* 
+
+4. On the console, run ``npm run serve-lab`` in order to launch the application 
+
+## Libs & Tooling
+
+1. [Angular 4.x](https://angular.io) 
+
+2. [Typescript](https://www.typescriptlang.org/) 
+
+3. [ionic 3.x](http://ionicframework.com) 
+
+4. [Sass](http://sass-lang.com/) 
+
+5. [TSLint](https://palantir.github.io/tslint/) 
--- src/app/core/db/todo.data-service.spec.ts
+++ src/app/core/db/todo.data-service.spec.ts
@@ -2,52 +2,52 @@
 // import {By} from "@angular/platform-browser";
 // import {DebugElement} from "@angular/core";
 
-import { Storage } from "@ionic/storage";
-
-import { TodoDataService } from "./todo.data-service";
-import { AppConstantsService } from "../app-constants.service";
-
-describe("TodoDataService", () => {
-
-  let service: TodoDataService;
-
-  beforeEach(() => {
-
-    let storage = new Storage({});
-    let appConstantsService = new AppConstantsService();
-
-    service = new TodoDataService(storage, appConstantsService);
-  });
-
-  it("#setupDb should resolve a promise after the setup", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#insertUpdateSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#deleteSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#getSections", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#insertUpdateTaskForSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#deleteTaskForSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#getTasksForSections", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-
-  it("#mergeSectionsTasksDb", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
-});
+// import { Storage } from "@ionic/storage";
+//
+// import { TodoDataService } from "./todo.data-service";
+// import { AppConstantsService } from "../app-constants.service";
+//
+// describe("TodoDataService", () => {
+//
+//   let service: TodoDataService;
+//
+//   beforeEach(() => {
+//
+//     let storage = new Storage({});
+//     let appConstantsService = new AppConstantsService();
+//
+//     service = new TodoDataService(storage, appConstantsService);
+//   });
+//
+//   it("#setupDb should resolve a promise after the setup", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#insertUpdateSection", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#deleteSection", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#getSections", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#insertUpdateTaskForSection", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#deleteTaskForSection", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#getTasksForSections", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+//
+//   it("#mergeSectionsTasksDb", () => {
+//     expect("a").toBe("a", "setupDb reject");
+//   });
+// });
