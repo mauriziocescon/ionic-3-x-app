97dc2e4a9868aed3eb4ec9f415eb00002d0ceabc
mau <maurizio.cescon@gmail.com>
2018-06-19 10:42:23 +0200

    improve serve

--- mock-server/server.js
+++ mock-server/server.js
@@ -4,14 +4,14 @@ const app = jsonServer.create();
 const dbUrl = require('./db/constants').dbUrl;
 const router = jsonServer.router(dbUrl);
 const middlewares = jsonServer.defaults({
-  static: 'build',
+  static: 'dist',
 });
 
 const delayMiddleware = require('./middlewares/delay');
 const errosMiddleware = require('./middlewares/errors');
 
 // set the port of our application
-// process.env.PORT lets the port be set by Heroku
+// process.env.PORT lets the port to be set by Heroku
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -24,8 +24,8 @@ app.use('/api', router);
 
 // Fallback on frontend routes
 app.get('*', (req, res) => {
-  // load the single view file (frontend will handle the page changes on the front-end)
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  // load index.html (frontend will handle page changes)
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // Start listening
